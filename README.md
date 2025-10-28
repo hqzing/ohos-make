1c92dffa746347f5da40b876e317f052ee51b073
hqzing <hqzing@outlook.com>
2025-10-28 14:47:32 +0800

    第一个版本

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # ohos-make
+本项目为 OpenHarmony 平台编译了 make，并发布预构建包。
+
+这个 make 不依赖 libc 以外的任何库，因此单独一个 make 二进制可执行文件就能运行。
+
+## 获取预构建包
+前往 [release 页面](https://github.com/Harmonybrew/ohos-make/releases) 获取。
+
+## 从源码构建
+需要用一台 Linux x64 服务器来运行项目里的 build.sh，以实现 make 的交叉编译。
+
+这里以 Ubuntu 24.04 x64 作为示例：
+```sh
+sudo apt update && sudo apt install -y build-essential unzip jq
+./build.sh
+```
---
+++ build.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+set -e
+
+# Setup ohos-sdk
+query_component() {
+  component=$1
+  curl -fsSL 'https://ci.openharmony.cn/api/daily_build/build/list/component' \
+    -H 'Accept: application/json, text/plain, */*' \
+    -H 'Content-Type: application/json' \
+    --data-raw '{"projectName":"openharmony","branch":"master","pageNum":1,"pageSize":10,"deviceLevel":"","component":"'${component}'","type":1,"startTime":"2025080100000000","endTime":"20990101235959","sortType":"","sortField":"","hardwareBoard":"","buildStatus":"success","buildFailReason":"","withDomain":1}'
+}
+sdk_download_url=$(query_component "ohos-sdk-public" | jq -r ".data.list.dataList[0].obsPath")
+curl $sdk_download_url -o ohos-sdk-public.tar.gz
+mkdir -p /opt/ohos-sdk
+tar -zxf ohos-sdk-public.tar.gz -C /opt/ohos-sdk
+cd /opt/ohos-sdk/linux
+unzip -q native-*.zip
+unzip -q toolchains-*.zip
+cd - >/dev/null
+
+# Setup env
+export OHOS_SDK=/opt/ohos-sdk/linux
+export AS=${OHOS_SDK}/native/llvm/bin/llvm-as
+export CC="${OHOS_SDK}/native/llvm/bin/clang --target=aarch64-linux-ohos"
+export CXX="${OHOS_SDK}/native/llvm/bin/clang++ --target=aarch64-linux-ohos"
+export LD=${OHOS_SDK}/native/llvm/bin/ld.lld
+export STRIP=${OHOS_SDK}/native/llvm/bin/llvm-strip
+export RANLIB=${OHOS_SDK}/native/llvm/bin/llvm-ranlib
+export OBJDUMP=${OHOS_SDK}/native/llvm/bin/llvm-objdump
+export OBJCOPY=${OHOS_SDK}/native/llvm/bin/llvm-objcopy
+export NM=${OHOS_SDK}/native/llvm/bin/llvm-nm
+export AR=${OHOS_SDK}/native/llvm/bin/llvm-ar
+export CFLAGS="-fPIC -D__MUSL__=1"
+export CXXFLAGS="-fPIC -D__MUSL__=1"
+
+# Build make
+curl -L -O https://mirrors.ustc.edu.cn/gnu/make/make-4.4.tar.gz
+tar -zxf make-4.4.tar.gz
+cd make-4.4
+./configure --prefix=/opt/make-4.4-ohos-arm64 --host=aarch64-linux
+make -j$(nproc)
+make install
+cp COPYING /opt/make-4.4-ohos-arm64/
+cd ..
+
+# Codesign
+/opt/ohos-sdk/ohos/toolchains/lib/binary-sign-tool sign -inFile /opt/make-4.4-ohos-arm64/bin/make -outFile /opt/make-4.4-ohos-arm64/bin/make -selfSign 1
+
+cd /opt/
+tar -zcf make-4.4-ohos-arm64.tar.gz make-4.4-ohos-arm64
